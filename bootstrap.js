b91cf2a8eda155f14e04b188e5231fd9edfe550a
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-02-03 01:01:55 +0500

    Lexer: Final fixes

--- bootstrap.js
+++ bootstrap.js
@@ -20,7 +20,7 @@ var tokenSeparators = [" ", "\n", ";", "(", ")", "{", "}", ","];
 var digits = "0123456789";
 var symbols = "!@#$%^&*-+\\|/=";
 var keywords = ["import", "as", "return", "cond", "false", "true", "maybe"];
-var operators = ["(", ")", "{", "}", ",", "+", "-", "*", "/", "=", "!=", ":=", "..", "->", "<", ">"];
+var operators = ["(", ")", "{", "}", ",", "+", "-", "*", "/", "=", "!=", ":=", "..", "->", "->@", "<", ">"];
 
 function addToken(index) {
   var type = TokenType.IDENTIFIER;
@@ -91,7 +91,7 @@ for (var i = 0; i < code.length; i++) {
     currentToken += char;
     continue;
   }
-  if (tokenSeparators.includes(char) || (currentToken.at(-1) == "-" && digits.includes(char) && tokens.at(-1) && (tokens.at(-1).type == TokenType.NUMBER || tokens.at(-1).type == TokenType.IDENTIFIER || code.slice(tokens.at(-1).start, tokens.at(-1).start + tokens.at(-1).length) == ")")) || (currentToken.at(-1) != "-" && canBeIdentifier(currentToken.at(-1)) != canBeIdentifier(char) && !(isNumber(currentToken) && char == ".") && !(isNumber(currentToken) && currentToken.at(-1) == "." && digits.includes(char))) || (isNumber(currentToken) && currentToken.includes(".") && char == ".") || (currentToken.at(-1) == "-" && canBeIdentifier(char) && !digits.includes(char)) || (currentToken.length && !currentToken.split("").find(char2 => !symbols.includes(char2)) && char == "-")) {
+  if (tokenSeparators.includes(char) || (currentToken.at(-1) == "-" && digits.includes(char) && tokens.at(-1) && (tokens.at(-1).type == TokenType.NUMBER || tokens.at(-1).type == TokenType.IDENTIFIER || code.slice(tokens.at(-1).start, tokens.at(-1).start + tokens.at(-1).length) == ")")) || (currentToken.at(-1) != "-" && canBeIdentifier(currentToken.at(-1)) != canBeIdentifier(char) && !(isNumber(currentToken) && char == ".") && !(isNumber(currentToken) && currentToken.at(-1) == "." && digits.includes(char))) || (isNumber(currentToken) && currentToken.includes(".") && char == ".") || (currentToken.at(-1) == "-" && canBeIdentifier(char) && !digits.includes(char)) || (currentToken.length && !currentToken.split("").find(char2 => !symbols.includes(char2)) && char == "-") || (currentToken == "->" && char == "-")) {
     if (isNumber(currentToken) && currentToken.at(-1) == "." && char == ".") {
       currentToken = currentToken.slice(0, -1);
       addToken(i - 1);
