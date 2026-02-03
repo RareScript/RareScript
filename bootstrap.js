5e9e07722d4d539428f97edd4878211a70f9730b
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-02-03 17:35:31 +0500

    Refactor everything

--- bootstrap.js
+++ bootstrap.js
@@ -1,12 +1,3 @@
-var fs = require("fs");
-var code = fs.readFileSync(process.argv[2]).toString("utf-8");
-
-code = code.split("\r\n").join("\n");
-
-var tokens = [];
-var currentToken = "";
-var isString = false;
-
 var TokenType = {
   "SEPARATOR": 0,
   "IDENTIFIER": 1,
@@ -16,137 +7,154 @@ var TokenType = {
   "STRING": 5,
   "NUMBER": 6
 };
-var tokenSeparators = [" ", "\n", ";", "(", ")", "{", "}", ","];
-var digits = "0123456789";
-var symbols = "!@#$%^&*-+\\|/=";
-var keywords = ["import", "as", "return", "cond", "false", "true", "maybe"];
-var operators = ["(", ")", "{", "}", ",", "+", "-", "*", "/", "%", "=", "!=", ":=", "..", "->", "->@", "<", ">"];
 
-function addToken(index) {
-  var type = TokenType.IDENTIFIER;
-  if (currentToken == "\n" || currentToken == ";") {
-    type = TokenType.SEPARATOR;
-  }
-  if (keywords.includes(currentToken)) {
-    type = TokenType.KEYWORD;
-  }
-  if (currentToken.startsWith("'") && currentToken.endsWith("'")) {
-    type = TokenType.CHAR;
-  }
-  if (currentToken.startsWith("\"") && currentToken.endsWith("\"")) {
-    type = TokenType.STRING;
-  }
-  if (isNumber(currentToken)) {
-    type = TokenType.NUMBER;
-  }
-  if (operators.includes(currentToken)) {
-    type = TokenType.OPERATOR;
-  }
-  tokens.push({
-    type,
-    "start": index - currentToken.length,
-    "length": currentToken.length
-  });
-}
+function lexer(code) {
+  var tokens = [];
+  var currentToken = "";
+  var isString = false;
+  var tokenSeparators = [" ", "\n", ";", "(", ")", "{", "}", ","];
+  var digits = "0123456789";
+  var symbols = "!@#$%^&*-+\\|/=";
+  var keywords = ["import", "as", "return", "cond", "false", "true", "maybe", "and", "or"];
+  var operators = ["(", ")", "{", "}", ",", "+", "-", "*", "/", "%", "=", "!=", ":=", "..", "->", "->@", "<", ">"];
 
-function canBeIdentifier(data) {
-  if (digits.includes(data)) {
-    return !!currentToken.length;
-  }
-  if (`${symbols}(){}.,<>'"\``.includes(data)) {
-    return false;
-  }
-  return true;
-}
-
-function isNumber(data) {
-  return (digits.includes(data[0]) || (data[0] == "-" && digits.includes(data[1])));
-}
-
-for (var i = 0; i < code.length; i++) {
-  var char = code[i];
-  if (char == "'") {
-    if (code[i + 2] != "'") {
-      // TODO: Errors
-      break;
+  function addToken(index) {
+    var type = TokenType.IDENTIFIER;
+    if (currentToken == "\n" || currentToken == ";") {
+      type = TokenType.SEPARATOR;
     }
-    currentToken += char;
-    currentToken += code[i + 1];
-    currentToken += char;
-    i += 2;
-    addToken(i + 1);
-    currentToken = "";
-    continue;
+    if (keywords.includes(currentToken)) {
+      type = TokenType.KEYWORD;
+    }
+    if (currentToken.startsWith("'") && currentToken.endsWith("'")) {
+      type = TokenType.CHAR;
+    }
+    if (currentToken.startsWith("\"") && currentToken.endsWith("\"")) {
+      type = TokenType.STRING;
+    }
+    if (isNumber(currentToken)) {
+      type = TokenType.NUMBER;
+    }
+    if (operators.includes(currentToken)) {
+      type = TokenType.OPERATOR;
+    }
+    tokens.push({
+      type,
+      "start": index - currentToken.length,
+      "length": currentToken.length
+    });
   }
-  if (char == "\"") {
-    isString = !isString;
-    if (!isString) {
+
+  function canBeIdentifier(data) {
+    if (digits.includes(data)) {
+      return !!currentToken.length;
+    }
+    if (`${symbols}(){}.,<>'"\``.includes(data)) {
+      return false;
+    }
+    return true;
+  }
+
+  function isNumber(data) {
+    return (digits.includes(data[0]) || (data[0] == "-" && digits.includes(data[1])));
+  }
+
+  for (var i = 0; i < code.length; i++) {
+    var char = code[i];
+    if (char == "'") {
+      if (code[i + 2] != "'") {
+        // TODO: Errors
+        break;
+      }
       currentToken += char;
+      currentToken += code[i + 1];
+      currentToken += char;
+      i += 2;
       addToken(i + 1);
       currentToken = "";
       continue;
     }
-  }
-  if (isString) {
-    currentToken += char;
-    continue;
-  }
-  if (currentToken == "%%") {
-    currentToken = "";
-    i += code.slice(i).indexOf("\n");
-    continue;
-  }
-  if (tokenSeparators.includes(char) || (currentToken.at(-1) == "-" && digits.includes(char) && tokens.at(-1) && (tokens.at(-1).type == TokenType.NUMBER || tokens.at(-1).type == TokenType.IDENTIFIER || code.slice(tokens.at(-1).start, tokens.at(-1).start + tokens.at(-1).length) == ")")) || (currentToken.at(-1) != "-" && canBeIdentifier(currentToken.at(-1)) != canBeIdentifier(char) && !(isNumber(currentToken) && char == ".") && !(isNumber(currentToken) && currentToken.at(-1) == "." && digits.includes(char))) || (isNumber(currentToken) && currentToken.includes(".") && char == ".") || (currentToken.at(-1) == "-" && canBeIdentifier(char) && !digits.includes(char)) || (currentToken.length && !currentToken.split("").find(char2 => !symbols.includes(char2)) && char == "-") || (currentToken == "->" && char == "-")) {
-    if (isNumber(currentToken) && currentToken.at(-1) == "." && char == ".") {
-      currentToken = currentToken.slice(0, -1);
-      addToken(i - 1);
-      currentToken = "..";
-      continue;
-    }
-    if (currentToken.length) {
-      addToken(i);
-    }
-    currentToken = "";
-    if (char != " " && char != "\n") {
-      currentToken += char;
-      if (char == ";" || char == "(" || char == ")" || char == ",") {
+    if (char == "\"") {
+      isString = !isString;
+      if (!isString) {
+        currentToken += char;
         addToken(i + 1);
         currentToken = "";
+        continue;
       }
     }
-    continue;
+    if (isString) {
+      currentToken += char;
+      continue;
+    }
+    if (currentToken == "%%") {
+      currentToken = "";
+      i += code.slice(i).indexOf("\n");
+      continue;
+    }
+    if (tokenSeparators.includes(char) || (currentToken.at(-1) == "-" && digits.includes(char) && tokens.at(-1) && (tokens.at(-1).type == TokenType.NUMBER || tokens.at(-1).type == TokenType.IDENTIFIER || code.slice(tokens.at(-1).start, tokens.at(-1).start + tokens.at(-1).length) == ")")) || (currentToken.at(-1) != "-" && canBeIdentifier(currentToken.at(-1)) != canBeIdentifier(char) && !(isNumber(currentToken) && char == ".") && !(isNumber(currentToken) && currentToken.at(-1) == "." && digits.includes(char))) || (isNumber(currentToken) && currentToken.includes(".") && char == ".") || (currentToken.at(-1) == "-" && canBeIdentifier(char) && !digits.includes(char)) || (currentToken.length && !currentToken.split("").find(char2 => !symbols.includes(char2)) && char == "-") || (currentToken == "->" && char == "-")) {
+      if (isNumber(currentToken) && currentToken.at(-1) == "." && char == ".") {
+        currentToken = currentToken.slice(0, -1);
+        addToken(i - 1);
+        currentToken = "..";
+        continue;
+      }
+      if (currentToken.length) {
+        addToken(i);
+      }
+      currentToken = "";
+      if (char != " " && char != "\n") {
+        currentToken += char;
+        if (char == ";" || char == "(" || char == ")" || char == ",") {
+          addToken(i + 1);
+          currentToken = "";
+        }
+      }
+      continue;
+    }
+    currentToken += char;
   }
-  currentToken += char;
-}
-if (currentToken.length) {
-  addToken(code.length);
-}
-if (isString) {
-  // TODO: Errors
+  if (currentToken.length) {
+    addToken(code.length);
+  }
+  if (isString) {
+    // TODO: Errors
+  }
+
+  return tokens;
 }
 
-for (var token of tokens) {
-  var type = "[]";
-  if (token.type == TokenType.SEPARATOR) {
-    type = "[SEPARATOR]";
+function processCode(code, debug) {
+  code = code.split("\r\n").join("\n");
+
+  var tokens = lexer(code);
+  if (debug) {
+    console.log("\x1b[32m[DEBUG TOKENS VIEW]\x1b[0m");
+    console.log(tokens.map(token => {
+      var color = 37;
+      if (token.type == TokenType.SEPARATOR) {
+        color = 31;
+      }
+      if (token.type == TokenType.IDENTIFIER) {
+        color = 36;
+      }
+      if (token.type == TokenType.KEYWORD) {
+        color = 35;
+      }
+      if (token.type == TokenType.CHAR) {
+        color = 34;
+      }
+      if (token.type == TokenType.STRING) {
+        color = 32;
+      }
+      if (token.type == TokenType.NUMBER) {
+        color = 33;
+      }
+      return `\x1b[${color}m${code.slice(token.start, token.start + token.length)}\x1b[0m`;
+    }).join(" "));
   }
-  if (token.type == TokenType.IDENTIFIER) {
-    type = "[IDENTIFIER]";
-  }
-  if (token.type == TokenType.KEYWORD) {
-    type = "[KEYWORD]";
-  }
-  if (token.type == TokenType.OPERATOR) {
-    type = "[OPERATOR]";
-  }
-  if (token.type == TokenType.CHAR) {
-    type = "[CHAR]";
-  }
-  if (token.type == TokenType.STRING) {
-    type = "[STRING]";
-  }
-  if (token.type == TokenType.NUMBER) {
-    type = "[NUMBER]";
-  }
-  console.log(`\x1b[32m${type}\x1b[0m`, JSON.stringify(code.slice(token.start, token.start + token.length)).slice(1, -1));
 }
+
+var fs = require("fs");
+var code = fs.readFileSync(process.argv[2]).toString("utf-8");
+processCode(code, true);
